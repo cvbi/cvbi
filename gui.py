e7d12f5a3cc064e386dae68730784df9219276d5
nilesh-patil <nilesh5760@gmail.com>
2019-02-10 03:35:02 -0500

    Update gui.py
    Updated gui helpers to create windows with user friendly functions.

--- gui.py
+++ gui.py
@@ -1,5 +1,4 @@
 import Tkinter as tk
-import tkFileDialog
 
 
 def create_window_from_list(object_list, window_title='Select one', w=500, h=800):
@@ -18,20 +17,16 @@ def create_window_from_list(object_list, window_title='Select one', w=500, h=800
     window.title(window_title)
     window.geometry(str(w)+"x"+str(h))
 
-    def close_window():
-        """Close window"""
-        window.destroy()
-
     object_select = tk.StringVar()
     object_select.set(object_list[0])
-
-    tk.Label(window, textvariable=object_select).pack()
+    header = tk.Label(master=window, textvariable=object_select)
+    header.pack()
 
     for item in object_list:
         selection_button = tk.Radiobutton(window, text=item, variable=object_select, value=item)
         selection_button.pack()
 
-    closing_button = tk.Button(master=window, text='Selection Complete', command=close_window)
+    closing_button = tk.Button(master=window, text='Selection Complete', command=window.destroy)
     closing_button.pack()
 
     window.mainloop()
@@ -41,9 +36,40 @@ def create_window_from_list(object_list, window_title='Select one', w=500, h=800
     return(object_string)
 
 
-def create_input_window(default='0', window_title='Provide input', w=400, h=200, window_text=None, valid_range=None):
+def create_window_for_multiple_selection(object_list, window_title='Select Atleast One', w=500, h=800):
     """
-    Create a selection window from provided list
+    Create a multiple selection window from provided list
+
+    :param object_list: List to create a button selector
+    :param window_title: Window title
+    :param w: width of the window, default = 500
+    :param h: height of the window, default = 800
+
+    :return: Creates a window to select from multiple
+    """
+
+    window = tk.Tk()
+    window.title(window_title)
+    window.geometry(str(w)+"x"+str(h))
+
+    values = {}
+    for item in object_list:
+        values[item] = tk.IntVar()
+        selection_button = tk.Checkbutton(master=window, text=item, variable=values[item], onvalue=item, offvalue=0)
+        selection_button.pack()
+
+    closing_button = tk.Button(master=window, text='Selection Complete', command=window.destroy)
+    closing_button.pack()
+
+    window.mainloop()
+
+    selection_list = [value.get() for key, value in values.iteritems() if value.get() != 0]
+    return(selection_list)
+
+
+def create_window_for_input(default='0', window_title='Provide input', w=400, h=200, window_text=None, valid_range=None):
+    """
+    Create a user input window
 
     :param default: Default value
     :param window_title: Title of the window
@@ -59,16 +85,12 @@ def create_input_window(default='0', window_title='Provide input', w=400, h=200,
     window.title(window_title)
     window.geometry(str(w)+"x"+str(h))
 
-    def close_window():
-        """Close window"""
-        window.destroy()
-
     value = tk.StringVar()
     value.set(default)
-    value_entered = tk.Entry(window, textvariable=value, width=w-50, text='Input')
+    value_entered = tk.Entry(master=window, textvariable=value, width=w-50, text='Input')
     value_entered.pack()
 
-    closing_button = tk.Button(master=window, text='Input Complete', command=close_window)
+    closing_button = tk.Button(master=window, text='Input Complete', command=window.destroy)
     closing_button.pack()
 
     T = tk.Text(window, height=10, width=w)
@@ -94,7 +116,34 @@ def create_input_window(default='0', window_title='Provide input', w=400, h=200,
     return(value_string)
 
 
-def get_output_dir(window_title="Select Folder", w=200, h=200):
+def get_output_dir(window_title="Select Folder", initial_dir="~" , w=400, h=200):
+    """
+    Get output directory for your file.
+
+    :param window_title: Title of the window
+    :param initial_dir: Starting directory to save file.
+    :param w: Width of the window
+    :param h: Height of the window
+
+    :return: Creates a window to get string output directory
+    """
+
+    import tkFileDialog
+
+    window = tk.Tk()
+    window.title(window_title)
+    window.geometry(str(w)+"x"+str(h))
+
+    window.directory = tkFileDialog.askdirectory(initialdir=initial_dir, title=window_title)
+    output_dir = window.directory
+
+    closing_button = tk.Button(master=window, text='Directory Selection Complete', command=window.destroy)
+    closing_button.pack()
+    window.mainloop()
+
+    return(output_dir)\
+
+def get_input_file(window_title="Select File", w=400, h=200):
     """
     Get output directory for your file.
 
@@ -105,19 +154,19 @@ def get_output_dir(window_title="Select Folder", w=200, h=200):
     :return: Creates a window to get string output directory
     """
 
-    def close_window():
-
-        window.destroy()
+    import tkFileDialog
 
     window = tk.Tk()
     window.title(window_title)
     window.geometry(str(w)+"x"+str(h))
-    window.directory = tkFileDialog.askdirectory(initialdir="~", title=window_title)
-    output_dir = window.directory
 
-    closing_button = tk.Button(master=window, text='Directory Selection Complete', command=close_window)
+    window.filename = tkFileDialog.askopenfilename(initialdir="~", title=window_title)
+    output_file = window.filename
+
+    closing_button = tk.Button(master=window, text='File Selection Complete', command=window.destroy)
     closing_button.pack()
     window.mainloop()
 
-    return(output_dir)
+    return(output_file)
+
 
